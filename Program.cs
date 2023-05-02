7ec99bea88d68dc25495baabcf0e0ba28490c1a7
Vladimir <urkytsk3@gmail.com>
2023-05-03 06:03:01 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DomclickComplaint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
