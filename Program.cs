7e0d5369a5ddbed8bb25299cd930df88561f9aa8
Vladimir <urkytsk3@gmail.com>
2023-05-05 11:21:30 +0800

    Сделал метод выбора категории

--- Program.cs
+++ Program.cs
@@ -10,31 +10,43 @@ namespace DomclickComplaint
         {
             string? logFileName = DateTime.Now.ToString("HH-mm-ss");
 
-            Uri uri;
-
-            Console.WriteLine("Выберите рубрику: 1 - Квартиры, 2 - Комнаты");
-            string choice = Console.ReadLine();
-
-            if (choice == "1")
-            {
-                uri = new Uri("https://tomsk.domclick.ru/search?deal_type=sale&category=living&offer_type=flat&from=topline2020&address=d5883f07-6a8e-4ba2-b0de-c266d11dd0e4&aids=13667&offset=0");
-            }
-            else if (choice == "2")
-            {
-                uri = new Uri("http://example.com/rooms");
-            }
-            else
-            {
-                Console.WriteLine("Некорректный выбор!");
-                return;
-            }
-
-            Console.WriteLine($"Вы выбрали рубрику: {choice}");
-            Console.WriteLine($"Ссылка на рубрику: {uri}");
+            Uri uri = ChooseCategory();
 
             Complaint complaint = new(uri, logFileName);
 
             complaint.SendComplaint();
+        }
+
+        // Метод выбора категории
+        static Uri ChooseCategory()
+        {
+            Uri uri;
+
+            while (true)
+            {
+                Console.WriteLine("Выберите рубрику:");
+                Console.WriteLine("  1 - Квартиры");
+                Console.WriteLine("  2 - Комнаты");
+
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    uri = new Uri("https://tomsk.domclick.ru/search?deal_type=sale&category=living&offer_type=flat&from=topline2020&address=d5883f07-6a8e-4ba2-b0de-c266d11dd0e4&aids=13667&offset=0");
+                    break;
+                }
+                else if (choice == "2")
+                {
+                    uri = new Uri("https://tomsk.domclick.ru/search?category=living&deal_type=sale&offer_type=room&from=topline2020");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный выбор! Попробуйте еще раз.");
+                }
+            }
+
+            return uri;
         }
     }
 }
